bd5152c4c74d637ba4c7a650fc433d84c8c124c8
Aadithya2804 <113664862+Aadithya2804@users.noreply.github.com>
2024-02-20 06:01:40 -0500

    Add files via upload

---
+++ frontend/src/components/signup.jsx
@@ -0,0 +1,87 @@
+import  { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function SignupPage() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [address, setAddress] = useState('');
+  const [phonenumber, setPhonenumber] = useState('');
+
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    
+    console.log('Signing up with:', username,email, password,address,phonenumber);
+  };
+
+  return (
+    
+    <div>
+      
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignup}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          
+        </div>
+        <div>
+          <label htmlFor="address">Address</label>
+          <input
+            type="address"
+            id="address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="phonenumber">Phone no</label>
+          <input
+            type="phonenumber"
+            id="phonenumber"
+            value={phonenumber }
+            onChange={(e) => setPhonenumber (e.target.value)}
+            required
+          />
+        </div>
+
+        <br></br>
+        <button type="submit">Register</button>
+      </form>
+      {/* <p>Already have an account? <a href="/login">Login</a></p> */}
+      <p>Already have an account? <Link to="/">Login</Link></p>
+
+    </div>
+  );
+}
+
+export default SignupPage;
+
